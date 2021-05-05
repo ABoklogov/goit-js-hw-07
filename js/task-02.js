a3c29f6eaf18446d9b5d40f0b31daf28d7dc7b00
Alexey Boklogov <kashira87@mail.ru>
2021-05-05 22:26:20 +0300

    выполнил 2-3 задания

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+ingredients.forEach(element =>
+   ingredientsEl.appendChild(document.createElement('li')).textContent = element
+);
+
+
+
