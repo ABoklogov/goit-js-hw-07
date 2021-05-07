639896670d28f707aa6bbfc5ef93bbfc9ab2ac0f
Alexey Boklogov <kashira87@mail.ru>
2021-05-07 23:57:41 +0300

    почти сделал 8 задание

---
+++ js/task-08.js
@@ -0,0 +1,40 @@
+const inputEl = document.querySelector('[type="number"]');
+const btnCreateEl = document.querySelector('[data-action="render"');
+const btnClearEl = document.querySelector('[data-action="destroy"');
+const boxEl = document.querySelector('#boxes');
+
+btnCreateEl.addEventListener('click', createBoxes);
+ 
+function createBoxes(amount) {
+    amount = inputEl.value;
+   
+    boxEl.style.display = 'flex';
+    boxEl.style.flexWrap = 'wrap';
+    let sizeBox = 20;
+
+    for (let i = 0; i < amount; i += 1) {
+        const box = document.createElement("div");
+        const r = Math.floor(Math.random() * (256));
+        const g = Math.floor(Math.random() * (256));
+        const b = Math.floor(Math.random() * (256));
+        sizeBox += 10;
+
+        boxEl.appendChild(box);
+        box.style.width = `${sizeBox}px`;
+        box.style.height = `${sizeBox}px`;
+        box.style.margin = '10px';
+        box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    };
+
+    const box = boxEl.querySelectorAll('div')
+    btnClearEl.addEventListener('click', clearBoxes);
+    
+   
+function clearBoxes() {
+        console.log(box);
+        // box.remove()
+    // boxEl.removeChild(box);
+// box.removeChild()
+}
+};
+
