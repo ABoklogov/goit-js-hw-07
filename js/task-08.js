85364a2ac0c4b78f66a933f58a1a6cb5004e3fd4
Alexey Boklogov <kashira87@mail.ru>
2021-05-10 17:06:12 +0300

    доделал 8 задание

--- js/task-08.js
+++ js/task-08.js
@@ -4,14 +4,14 @@ const btnClearEl = document.querySelector('[data-action="destroy"');
 const boxEl = document.querySelector('#boxes');
 
 btnCreateEl.addEventListener('click', createBoxes);
+let sizeBox = 20;
 
 function createBoxes(amount) {
     amount = inputEl.value;
-   
+
     boxEl.style.display = 'flex';
     boxEl.style.flexWrap = 'wrap';
-    let sizeBox = 20;
-
+    
     for (let i = 0; i < amount; i += 1) {
         const box = document.createElement("div");
         const r = Math.floor(Math.random() * (256));
@@ -20,11 +20,17 @@ function createBoxes(amount) {
         sizeBox += 10;
 
         boxEl.appendChild(box);
+
         box.style.width = `${sizeBox}px`;
         box.style.height = `${sizeBox}px`;
         box.style.margin = '10px';
         box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        box.setAttribute('data-sizeBox', `${sizeBox}`);
     };
+    
+    if (boxEl.children.length !== 0) {
+        sizeBox = Number(boxEl.lastElementChild.getAttribute('data-sizeBox'));
+    } 
 
     btnClearEl.addEventListener('click', destroyBoxes);
 
@@ -32,10 +38,10 @@ function createBoxes(amount) {
 
     function destroyBoxes() {
         boxes.forEach(element => element.remove())
+        sizeBox = 20;
     };
 };
 
 
-     
-       
-    
+
+
