cc49ae4f3eae262987d7c38a68d4c9ed495da0ac
Alexey Boklogov <kashira87@mail.ru>
2021-05-12 13:06:33 +0300

    доделал 8 задание

--- js/task-08.js
+++ js/task-08.js
@@ -12,22 +12,24 @@ function createBoxes(amount) {
     boxEl.style.display = 'flex';
     boxEl.style.flexWrap = 'wrap';
     
-    for (let i = 0; i < amount; i += 1) {
-        const box = document.createElement("div");
-        const r = Math.floor(Math.random() * (256));
-        const g = Math.floor(Math.random() * (256));
-        const b = Math.floor(Math.random() * (256));
-        sizeBox += 10;
+    Array.from(Array(Number(amount)).keys())
+        .forEach(el => {
+            const box = document.createElement("div");
+            const r = Math.floor(Math.random() * (256));
+            const g = Math.floor(Math.random() * (256));
+            const b = Math.floor(Math.random() * (256));
+            sizeBox += 10;
 
-        boxEl.appendChild(box);
+            boxEl.appendChild(box);
 
-        box.style.width = `${sizeBox}px`;
-        box.style.height = `${sizeBox}px`;
-        box.style.margin = '10px';
-        box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        box.setAttribute('data-sizeBox', `${sizeBox}`);
-    };
-    
+            box.style.width = `${sizeBox}px`;
+            box.style.height = `${sizeBox}px`;
+            box.style.margin = '10px';
+            box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            box.setAttribute('data-sizeBox', `${sizeBox}`);
+        }
+        );
+
     if (boxEl.children.length !== 0) {
         sizeBox = Number(boxEl.lastElementChild.getAttribute('data-sizeBox'));
     } 
